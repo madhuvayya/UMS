a85de07d385628eb7428f6ea7a3a95cace5564b2
madhuvayya <madhu.vayya@gmail.com>
2020-08-22 22:29:54 +0530

    Added query to insert data user_info table

---
+++ dml.sql
@@ -0,0 +1,3 @@
+-- query to insert user data into user_info table
+INSERT INTO `ums`.`user_info`(`first_name`,`last_name`,`dob`,`gender`,`email`,`country`,`phone_number`,`address`,`user_name`,`password`,`role`,`status`,`creator_user`)
+VALUES('Madhu','V','1997-05-06','male','madhu.vayya@gmail.com','India',7684946313,'Hyderabad Telangana','admin','pwd123','admin','active','user');
